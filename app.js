3324ec72b32c324d6a416f42269546bed72dbb8f
Dutch <dutch@unviral.com>
2019-06-11 22:00:48 -0400

    Add web3 and show block number

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
+// Initialize a global web3 instance connecting to a remote node
+// We are using the Infura Ethereum node here, but you can use the URL of any provider. 
+// Latest web3.js 0.20.x version without cors issues is 0.20.6
+web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/zU4GTAQ0LjJNKddbyztc"));
+
 // Run when document has loaded
 window.addEventListener('DOMContentLoaded', function(e) {
-      console.log('DOM fully loaded and parsed');
+  // Get the current block number and verify the connection
+  let blockNumber = web3.eth.blockNumber;
+  document.querySelector("#block-height").innerHTML = blockNumber;
 });
