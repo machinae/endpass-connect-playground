a603713456ad3143c04ae8b8cd82d895662e2047
Dutch <dutch@unviral.com>
2019-06-20 01:01:46 -0400

    Initialize Endpass Connect per developer instructions

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 // Initialize a global web3 instance connecting to a remote node
 // We are using the Infura Ethereum node here, but you can use the URL of any provider. 
+const WEB3_PROVIDER = "https://mainnet.infura.io/zU4GTAQ0LjJNKddbyztc";
+
+// Client ID registered at https://vault.endpass.com
+const OAUTH_APP_ID = "a616a3e4-50bb-4eae-a5b1-0aeda6683bc9";
+
 // Latest web3.js 0.20.x version without cors issues is 0.20.6
-web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/zU4GTAQ0LjJNKddbyztc"));
+const web3 = new Web3(WEB3_PROVIDER);
+
+// Create a new Endpass Connect instance with our registered client id
+const connect = new EndpassConnect({
+  oauthClientId: OAUTH_APP_ID,
+});
+
+// Get the Endpass web3 provider
+const provider = connect.getProvider();
+
+// For compatibility with some Ethereum wallets
+window.ethereum = provider;
+
+// After web3 is loaded, set it to use our custom provider
+web3.setProvider(provider);
+
+// Set the global web3 object (optional, for compatibility with older dApp browsers)
+window.web3 = web3;
 
 // Run when document has loaded
 window.addEventListener('DOMContentLoaded', function(e) {
